3c119faac597bd6a0cd09db01b6c6d4a2c9545b6
Andrew Johnson <LimeyJohnson@gmail.com>
2013-02-06 10:12:00 -0800

    Adding etsy wrapper

---
+++ OfficeApp1Script/Etsy.cs
@@ -0,0 +1,18 @@
+// Class1.cs
+//
+
+using System;
+using System.Collections.Generic;
+using System.Html;
+using jQueryApi;
+using AgaveApi;
+using System.Runtime.CompilerServices;
+namespace OfficeApp1Script
+{
+    public static class Etsy
+    {
+        public static void SendRequest()
+        {
+        }
+    }
+}
