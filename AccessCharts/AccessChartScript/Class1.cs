3858855fce515068b0e0a1a2faacf079b52603b8
Andrew Johnson <LimeyJohnson@gmail.com>
2013-07-24 22:39:54 -0700

    Added in chart agave

---
+++ AccessCharts/AccessChartScript/Class1.cs
@@ -0,0 +1,19 @@
+// Class1.cs
+//
+
+using System;
+using System.Collections.Generic;
+using AgaveApi;
+namespace AccessChartScript
+{
+    public static class ChartScript
+    {
+        static ChartScript()
+        {
+            Office.Initialize = delegate(InitializationEnum harris)
+            {
+                
+            };
+        }
+    }
+}
---
+++ AccessCharts/AccessChartsWeb/Scripts/AccessChartScript.debug.js
@@ -0,0 +1,23 @@
+//! AccessChartScript.debug.js
+//
+
+(function($) {
+
+Type.registerNamespace('AccessChartScript');
+
+////////////////////////////////////////////////////////////////////////////////
+// AccessChartScript.Class1
+
+AccessChartScript.Class1 = function AccessChartScript_Class1() {
+}
+
+
+AccessChartScript.Class1.registerClass('AccessChartScript.Class1');
+(function () {
+    Office.initialize = function (harris) {
+
+    };
+})();
+})(jQuery);
+
+//! This script was generated using Script# v0.7.4.0
