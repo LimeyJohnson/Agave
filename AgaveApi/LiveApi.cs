59f1814397aa088826b131e9de7d8b36276bbb7f
LimeyJohnson <LimeyJohnson@gmail.com>
2013-09-07 14:36:05 -0700

    Getting the sdk in

---
+++ AgaveApi/LiveApi.cs
@@ -0,0 +1,85 @@
+// Class1.cs
+//
+
+using System;
+using System.Html;
+using System.Runtime.CompilerServices;
+using System.Html.Data;
+using System.Collections.Generic;
+namespace Live
+{
+
+    #region Classes
+    [Imported, IgnoreNamespace, ScriptName("WL")]
+    public static class LiveApi
+    {
+        public extern static PromiseObject Login(Dictionary<string, string> args);
+        public extern static PromiseObject Login(LoginOptions options);
+        public extern static PromiseObject Api(Dictionary<string, string> args);
+        public extern static PromiseObject Init(InitOptions options);
+        public extern static PromiseObject Ui(UiOptions options);
+        [PreserveCase]
+        public static EventObject Event;
+    }
+    [Imported, IgnoreNamespace, ScriptName("Object")]
+    public class UiOptions
+    {
+        public string name;
+        public string element;
+    }
+    [Imported, IgnoreNamespace, ScriptName("Object")]
+    public class InitOptions
+    {
+
+        public string client_id;
+        public string redirect_uri;
+        public string scope;
+        public string response_type;
+        public bool logging;
+
+    }
+    [Imported, IgnoreNamespace]
+    public class EventObject
+    {
+        public extern void subscribe(string authType, Action<LoginResponse> callback);
+    }
+    [Imported, IgnoreNamespace, ScriptName("Object")]
+    public class LoginOptions
+    {
+        public extern LoginScope Scope { set; }
+    }
+    [Imported, IgnoreNamespace, ScriptName("Object")]
+    public class PromiseObject
+    {
+        public extern void Then(Action<LoginResponse> action);
+        public extern void Then(Action<LoginResponse> success, Action<LoginResponse> failure);
+        public extern void Then(Action<Dictionary<string, string>> action);
+        public extern void Then(Action action);
+
+    }
+    [Imported, IgnoreNamespace, ScriptName("Object")]
+    public class LoginResponse
+    {
+
+    }
+
+    [Imported, IgnoreNamespace, ScriptName("Object")]
+    public class ApiOptions
+    {
+        public string Path;
+        public string Method;
+    }
+
+
+
+    #endregion
+    #region Enums
+    [Imported, IgnoreNamespace, ScriptName("")]
+    public enum LoginScope
+    {
+        [PreserveCase, ScriptName("wl.skydrive_update")]
+        SkydriveUpdate,
+    }
+
+    #endregion
+}
--- SkyDrive/SkyDriveScript/SkyDrive.cs
+++ SkyDrive/SkyDriveScript/SkyDrive.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Html;
 using jQueryApi;
 using AgaveApi;
+using Live;
 namespace SkyDriveScript
 {
 
@@ -15,8 +16,41 @@ namespace SkyDriveScript
         {
             Office.Initialize = delegate(InitializationEnum initReason)
             {
-                jQuery.Select("#test").Html("I Like Cheese");
+                LiveApi.Event.subscribe("auth.login", OnLogon);
+                InitOptions initOptions = new InitOptions();
+                initOptions.client_id = "000000004C100093";
+                initOptions.redirect_uri = "http://filesagave.azurewebsites.net/App/Home/callback.html";
+                initOptions.scope = "wl.signin";
+                initOptions.response_type = "token";
+                initOptions.logging = true;
+                LiveApi.Init(initOptions).Then(OnSuccess, OnFailure) ;
+
+                UiOptions uiOptions = new UiOptions();
+                uiOptions.name = "signin";
+                uiOptions.element = "signin";
+                LiveApi.Ui(uiOptions);
+                
+
+                
             };
+        }
+        public static void OnLogon(LoginResponse response)
+        {
+            LiveApi.Login(new Dictionary<string, string>("scope", new string[] { "wl.signin", "wl.basic", "wl.birthday", "wl.emails" })).Then(delegate(LoginResponse loginResponse)
+            {
+                LiveApi.Api(new Dictionary<string, string>("path", "me", "method", "GET")).Then(delegate(Dictionary<string, string> apiResponse)
+                {
+                    jQuery.Select("#first_name").Text(apiResponse["first_name"]);
+                });
+            });
+        }
+        public static void OnFailure(LoginResponse failResponse)
+        {
+            jQuery.Select("#first_name").Text(failResponse.ToString());
+        }
+        public static void OnSuccess(LoginResponse successResponse)
+        {
+            jQuery.Select("#first_name").Text(successResponse.ToString());
         }
     }
 }
