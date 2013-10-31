948e53015c23f512d294ea18482fe4a647fd0d2e
LimeyJohnson <LimeyJohnson@gmail.com>
2013-10-30 19:20:46 -0700

    committing some files I found on my home laptop

--- SkyDrive/SkyDriveScript/SkyDrive.cs
+++ SkyDrive/SkyDriveScript/SkyDrive.cs
@@ -16,23 +16,24 @@ namespace SkyDriveScript
         {
             Office.Initialize = delegate(InitializationEnum initReason)
             {
-                
-                InitOptions initOptions = new InitOptions();
-                initOptions.client_id = "000000004C100093";
-                initOptions.redirect_uri = "http://filesagave.azurewebsites.net/App/callback.html";
-                initOptions.scope = "wl.signin";
-                initOptions.response_type = "code";
-                initOptions.logging = true;
-                LiveApi.Init(initOptions).Then(OnSuccess, OnFailure) ;
-                LiveApi.Event.subscribe("auth.login", OnLogon);
-                LiveApi.Event.subscribe("wl.log", OnLog);
-                UiOptions uiOptions = new UiOptions();
-                uiOptions.name = "signin";
-                uiOptions.element = "signin";
-                uiOptions.brand = "skydrive";
-                uiOptions.onloggedin = new Action<LoginResponse>(GetName);
-                LiveApi.Ui(uiOptions);
-                
+                jQuery.OnDocumentReady(delegate()
+                {
+                    InitOptions initOptions = new InitOptions();
+                    initOptions.client_id = "000000004C100093";
+                    initOptions.redirect_uri = "http://filesagave.azurewebsites.net/App/callback.html";
+                    initOptions.scope = "wl.signin";
+                    initOptions.response_type = "code";
+                    initOptions.logging = true;
+                    LiveApi.Init(initOptions).Then(OnSuccess, OnFailure);
+                    LiveApi.Event.subscribe("auth.login", OnLogon);
+                    LiveApi.Event.subscribe("wl.log", OnLog);
+                    UiOptions uiOptions = new UiOptions();
+                    uiOptions.name = "signin";
+                    uiOptions.element = "signin";
+                    uiOptions.brand = "skydrive";
+                    uiOptions.onloggedin = new Action<LoginResponse>(GetName);
+                    LiveApi.Ui(uiOptions);
+                });
 
                 
             };
